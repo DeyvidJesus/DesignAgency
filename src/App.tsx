5ee0861a10d655b15a7589834f005cb3c77b9464
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-22 17:17:24 -0300

    Configuração inicial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from './components/Header.tsx'
+import { Footer } from './components/Footer.tsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='bg-light-green h-screen'>
+
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
