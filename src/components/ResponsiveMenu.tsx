4928655a73c643dc9be1f47d216c39d34ae712eb
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-26 10:05:16 -0300

    Update ResponsiveMenu.tsx

--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -14,11 +14,10 @@ export function ResponsiveMenu() {
         {isOpened &&
           <nav className="fixed bg-light-green w-screen left-0 top-14 h-screen flex flex-col items-center pt-8 text-xl font-medium">
             <div className="flex flex-col h-1/2 justify-between items-center">
-              <a className="text-black no-underline mb-4 hover:underline flex items-center justify-between" href="#Services">Services <img src="/dropdown_arrow.svg" alt="" /></a>
-              <a className="text-black no-underline mb-4 hover:underline" href="#About">About Us</a>
-              <a className="text-black no-underline mb-4 hover:underline" href="#Contact">Contact Us</a>
-              <button className="bg-transparent text-black mb-4 border-2 py-2 px-5 rounded-lg border-dark-green">Login</button>
-              <button className="bg-dark-green text-white py-2 px-5 rounded-lg border-none">Register</button>
+              <a className="text-black no-underline hover:underline" href="#WhatWeDo">What We Do</a>
+              <a className="text-black no-underline hover:underline" href="#OurPortfolio">Our Portfolio</a>
+              <a className="text-black no-underline hover:underline" href="#Testimonials">Testimonials</a>
+              <a className="text-black no-underline hover:underline" href="#Subscribe">Subscribe</a>
             </div>
           </nav>
         }
