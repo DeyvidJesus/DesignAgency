2c96f285c28993f73d09b2bb8324b9f6cb3e296f
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-26 10:08:36 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ResponsiveMenu } from "./ResponsiveMenu";
 
 export function Header() {
   return (
-    <header className="max-w-full fixed w-screen flex items-center justify-between sm:px-10 lg:px-20 bg-light-green h-20">
+    <header className="max-w-full fixed z-[5000] w-screen flex items-center justify-between sm:px-10 lg:px-20 bg-light-green h-20">
       <img src="/logo.svg" alt="" className="w-40" />
       <ResponsiveMenu />
       <nav className="sm:hidden md:flex md:w-2/3 justify-between items-center font-medium">
